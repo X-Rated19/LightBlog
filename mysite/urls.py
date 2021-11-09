8ebc36cdd5472d9a2b5ddfbcef39900ddd986a26
X-Rated19 <atabekov.daulet.kz@gmail.com>
2021-11-09 21:25:14 +0600

    quotes

--- mysite/urls.py
+++ mysite/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
+    path('quote', views.quote_list, name='quote_list'),
 ]
--- mysite/views.py
+++ mysite/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 from django.utils import timezone
-from .models import Post
+from .models import Post, Quote
+
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'mysite/post_list.html', {'posts': posts})
+
+def quote_list(request):
+    quotes = Quote.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'mysite/quote_list.html', {'quotes': quotes})
+
 
